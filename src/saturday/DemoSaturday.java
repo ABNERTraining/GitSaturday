7b3bf4cbd87de3425574fd7948600e72b8c13cb3
ABNER <ABNER@ABNER.lan>
2021-10-02 10:58:58 -0400

    new changes

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -1,13 +1,18 @@
 package saturday;
 
 public class DemoSaturday {
+	public static void multiply(int a, int b) {
+		System.out.println(a * b);
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Saturday  Demo...");
 	}
-	
-	
-	//this is coming from github
-	public static void divNum(int a, int b){
-		System.out.println(a/b);
 
+	// this is coming from github
+	public static void divNum(int a, int b) {
+		System.out.println(a / b);
+
+	}
 }

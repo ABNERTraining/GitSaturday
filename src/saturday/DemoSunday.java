4eb34eb6a8490828bd127362db92154617011a7b
ABNERTraining <91448888+ABNERTraining@users.noreply.github.com>
2021-10-02 10:46:08 -0400

    Update DemoSunday.java
    added addNum0 method

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -4,5 +4,7 @@ public class DemoSunday {
 	public static void main(String[] args) {
 		System.out.println("Sunday Demo...");
 	}
-
+	//coming from github
+public static void addNum(int a, int b){
+	System.out.println(a+b);
 }
